ea99ec0606c196177c34ee0a7b74ceef45eb846c
yancyuu <56645657+yancyuu@users.noreply.github.com>
2023-05-30 19:39:13 +0800

    优化环境变量传递

--- components/Chatbar/components/Login.tsx
+++ components/Chatbar/components/Login.tsx
@@ -3,6 +3,7 @@ import { IconLogin, IconLogout } from '@tabler/icons-react';
 import { SidebarButton } from '@/components/Sidebar/SidebarButton';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
+import { USER_MANAGER_HOST } from '@/utils/app/const';
 
 
 export const LoginPanel = () => {
@@ -10,8 +11,8 @@ export const LoginPanel = () => {
   const { t } = useTranslation('sidebar');
   const [isLogin, setIsLogin] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
-  const login_url = `${process.env.USER_MANAGER_HOST}/user/login`;
-  const check_token_url = `${process.env.USER_MANAGER_HOST}/user/check-token`;
+  const login_url = `${USER_MANAGER_HOST}/user/login`;
+  const check_token_url = `${USER_MANAGER_HOST}/user/check-token`;
 
   const [isPhoneLogin, setIsPhoneLogin] = useState(true);
   const [phone, setPhone] = useState('');
